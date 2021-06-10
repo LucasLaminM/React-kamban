d8c4437f5b24ecc2795cd5fde1778a448c9bb96b
LucasLaminM <71229329+LucasLaminM@users.noreply.github.com>
2021-06-10 09:54:59 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Olá mundo</h1>
+      <ul className="lista">
+        <li>Aprendemos react</li>
+        <li>Aprendemos componentes</li>
+      </ul>
+    </div>
+  );
+}
